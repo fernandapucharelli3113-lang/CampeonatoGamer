739c5b66fbf1695e5b529d7392da9e55560e3944
FERNANDA PUCHARELLI SANTOS <26171647@edu119.local>
2026-09-24 16:55:57 -0300

    txt

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ let PARTIDAS = [
         jogo: "Arena Pixel",
         timeA: "Falcões",
         timeB: "Dragões",
-        prontoA: 3,
-        prontoB: 2,
+        pontoA: 3,
+        pontoB: 2,
         status: "finalizado"
     },
     {
@@ -20,8 +20,8 @@ let PARTIDAS = [
         jogo: "Corrida Turbo",
         timeA: "Lobos",
         timeB: "Falcões",
-        prontoA: 0,
-        prontoB: 0,
+        pontoA: 0,
+        pontoB: 0,
         status: "agendada"
     },
 ]
@@ -41,6 +41,76 @@ app.get("/partidas/:id",(req,res)=>{
 
     res.status(200).json(partida);
 });
+ app.post("/partidas",(req,res)=>{
+    const { jogo, timeA, timeB} = req.body;
+
+    if(!jogo || !timeA || !timeB){
+        return res.status(400).json({
+            mensagem: "Informe jogo, timeA e timeB"
+        });
+    }
+    if(timeA.trim().tolowerCase() === timeB.trim().tolowerCase()){
+        return res.status(400).json({
+            mensagem: "Os times devem ser diferentes"
+        });
+    }
+    const novoId = PARTIDAS.length > 0
+    ?Math.max(...PARTIDAS.map(p => p.id)) +1 : 1;
+
+    const novaPartida ={
+        id: novoID, 
+        jogo: jogo.trim(),
+        timeA: timeA.trim(),
+        timeB: timeB.trim(),
+        pontoA: 0,
+        pontoB: 0,
+        status: "agendada",
+    };
+
+    PARTIDAS.push(novaPartida);
+    res.status(201).json({
+        mensagem: "Partida Cadastrada",
+        partida: novaPartida
+    });
+});
+
+app.put("/partidas/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const indice = PARTIDAS.findIndex(p => p.id === id);
+
+    if(indice === -1){
+        return res.status(404).json({
+            msg: "Partida não encontrada"});
+    }
+
+    const {jogo, timeA, timeB, pontoA, pontoB, status} = req.body;
+
+    if(!jogo || !timeA || !timeB 
+        || !Number.isInteger(pontoA)
+        || !Number.isInteger(pontoB)
+        || pontoA < 0 || pontoB < 0
+        || !["agendada", "finalizada"].includes(status)
+    ){
+        return res.status(400).json({
+            mensagem: "Envio jogo, times, placares válidos e status"
+        });
+    }
+    
+    PARTIDAS[indice] = {
+        id,
+        jogo,
+        timeA,
+        timeB,
+        pontoA,
+        pontoB,
+        status
+    };
+    res.status(200).json({
+        mensagem: "Partida atualizada",
+        partida: PARTIDAS[indice]
+    })
+});
+
  const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
